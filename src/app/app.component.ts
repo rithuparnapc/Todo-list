c9a50d018690b6ddb51908571ea9e20204060385
Rathuparnna PC <rithuparna2631@gmail.com>
2022-12-27 23:38:32 +0530

    Todolist

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todolist';
+
+  list:any[]=[];
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item})
+    console.log(this.list);
+    
+  }
+  removeTask(id:number){
+    console.log(id);
+    this.list=this.list.filter(item=>item.id!==id);
+    
+  }
+}
